9328ab1ea2043c92950c33c1d5ad8cea7d2da5ae
Marhlon Korb <marhlonkorb@hotmail.com>
2023-04-13 00:17:34 -0300

    Commit 1
    - Criado projeto e adicionada as dependências do Material;
    - Criado campo de input com validação que recebe valor digitado no submit do formulário.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-estudos';
+export class AppComponent implements OnInit {
+
+  public form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  public inicializaFormulario(){
+    this.form = this.formBuilder.group({
+      name: '',
+    });
+
+  }
+
+  ngOnInit(): void {
+    this.inicializaFormulario();
+    this.form = new FormGroup({
+      name: new FormControl('', Validators.required),
+    })
+  }
+
+  onSubmit(){
+    console.log(this.form.controls['name'].value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialExampleModule } from 'src/Material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MaterialExampleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
